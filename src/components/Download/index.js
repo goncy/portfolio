fa0b6921ecc770bed1255bbc755d8014c82185b1
goncy <gonzalo.pozzo4@gmail.com>
2017-10-03 08:30:24 -0300

    Fix image border style

--- src/components/Download/index.js
+++ src/components/Download/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { compose } from 'recompose'
+import styled from 'styled-components'
 
 import {constants} from '../../data'
+
+const ImageLink = styled.a`
+  height: 86px;
+`
 
 export const Download = ({ visitor }) => (
   <div className='tc pa2 pa4-ns'>
     <div className='mw9 center pa2'>
       <h1 className='f3 mt0 mb3'>Need the old and trustable PDF?</h1>
-      <a style={{height: 84}} target='_blank' href={constants.CV_LINK} className='f6 f5-ns b dim br1 ba dib pointer no-underline'>
+      <ImageLink target='_blank' href={constants.CV_LINK} className='f6 f5-ns b dim br1 ba dib pointer no-underline'>
         <img width='128' src={constants.PERHAPS_IMG} alt='Perhaps' />
-      </a>
+      </ImageLink>
     </div>
   </div>
 )
